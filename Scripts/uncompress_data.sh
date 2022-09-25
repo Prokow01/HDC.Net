cedbf437ceb15baa98b336512c212c7983405b96
Prokow01 <peterrokowski@gmail.com>
2022-09-24 20:33:25 -0400

    adding the decompress data step for light git storage

--- Scripts/uncompress_data.sh
+++ Scripts/uncompress_data.sh
@@ -1,3 +1,16 @@
 ## Uncompress_Data.sh
 #   - Shell script to prepare the tar/gzipped data folders for the tests
+#
+# Author: Peter Rokowski (prokow01@villanova.edu)
 
+DATA_PATH="../Data/";
+DATA_TAR="mnist_data.tgz";
+#if [ -z "$DATA_TAR" ]; then
+#    echo "bad param: DATA_TAR - not provided or null";
+#    exit 1;
+#fi
+
+
+# make more complicated at some point
+cd $DATA_PATH;
+tar -xvf $DATA_TAR;
