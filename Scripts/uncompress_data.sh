ef82b5de1c5f05a2b6bf3eb5cd6de36dc8a7be16
Prokow01 <peterrokowski@gmail.com>
2022-09-24 20:57:36 -0400

    making the uncompress a little better

--- Scripts/uncompress_data.sh
+++ Scripts/uncompress_data.sh
@@ -3,14 +3,25 @@
 #
 # Author: Peter Rokowski (prokow01@villanova.edu)
 
-DATA_PATH="../Data/";
-DATA_TAR="mnist_data.tgz";
+
+PROJ_ROOT="..";
+TARGET_DIR="$PROJ_ROOT/HyperSharp/MNIST";
+DATA_PATH="$PROJ_ROOT/Data/";
+
+DATA_NAME="mnist_data";
+DATA_TAR="$DATA_NAME.tgz";
 #if [ -z "$DATA_TAR" ]; then
 #    echo "bad param: DATA_TAR - not provided or null";
 #    exit 1;
 #fi
 
+echo "creating data_dir $TARGET_DIR";
+mkdir -p $TARGET_DIR;
 
 # make more complicated at some point
 cd $DATA_PATH;
 tar -xvf $DATA_TAR;
+
+mv $DATA_NAME/* $TARGET_DIR/.;
+rm -rf $DATA_NAME;
+
